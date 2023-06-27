099aed93d3e9da13c2fdc3db0ed1c210c200280d
Shuma01 <shudarima@gmail.com>
2023-06-27 21:37:33 +0900

    dbの理解

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request ,redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(100))
+    password= db.Column(db.String(30), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('hello.html')
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+    else:
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        new_post = Post(name=name, email=email, password=password)
 
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
